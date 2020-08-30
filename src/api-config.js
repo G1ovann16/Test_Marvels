99470e2b34e7523e17737a026ee2fa6ad8134d1a
G1ovann16 <60437581+G1ovann16@users.noreply.github.com>
2020-08-30 17:54:31 +0200

    fix

---
+++ src/api-config.js
@@ -0,0 +1,3 @@
+export const BASE_URL = 'http://gateway.marvel.com/v1/public/';
+export const CHARACTER_LIST = 'characters?ts=1&apikey=c9540302ac1498d6ccb272151949cb35&hash=0caa4fc8940efcf700f4771e6412699e'
+export const CHARACTER_ROUTE = 'characters/1009220?ts=1&apikey=c9540302ac1498d6ccb272151949cb35&hash=0caa4fc8940efcf700f4771e6412699e'
