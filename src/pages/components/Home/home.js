41e2eefa7331a82d65dcd1c2f6c6a3051d723cac
G1ovann16 <60437581+G1ovann16@users.noreply.github.com>
2020-08-30 21:40:39 +0200

    made design

--- src/pages/components/Home/home.js
+++ src/pages/components/Home/home.js
@@ -11,62 +11,105 @@ useEffect(() => {
     stories();
     
 }, []);
+
+const  comic = () =>({
+    
+})
+
     return (
-        <div className="Home container-fluid">
-            <div className="col">
-                <div className="row">
-                {props.hero?.name}
-                {props.hero?.description}
-                </div> 
-                <div className="row">
-                <img src={props.hero?.thumbnail?.path+'.'+props.hero?.thumbnail?.extension}/>
-                
-                </div>   
+    <div>    
+      <div class="bg-image">
+        <div class="container">
+          <div class="row">
+            <div class="col-6 my-auto">
+              <h1>Captain America</h1>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos pariatur quod totam doloremque, nulla veritatis dolorem? Perferendis, vero. Magni corrupti odit rerum maiores assumenda quam, quos repudiandae architecto in saepe.</p>
+              <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Primary link</a>
+              <a href="#" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Link</a>
             </div>
-                <p>
-            </p>
+            <div class="col-6">
+              <img src="captain.png" alt="" width="100%"/>
+            </div>
+          </div>
         </div>
+      </div>
+  
+      <div class="container mt-3">
+        <div class="text-center">
+          <h3 class="subtitle">Best Movies</h3>
+          <p class="mb-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit dolore cupiditate vel ut quo alias.</p>
+          <div class="row">
+            <div class="col-4">
+              <img src={'https://i.pinimg.com/736x/ba/9b/36/ba9b3623c9a639296e85f7ff09c3c205.jpg'} alt="" width="100%"/>
+              <h6 class="mt-3">Capitan America 1</h6>
+              <p class="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi rem earum recusandae consectetur, eum amet odit beatae nihil nam dolorum a ipsa autem, optio praesentium, iure officiis iste dolorem fuga?</p>
+              <h6><span>2020</span></h6>
+            </div>
+            <div class="col-4">
+              <img src={'../images/movie.jpg'} alt="" width="100%"/>
+              <h6 class="mt-3">Capitan America 2</h6>
+              <p class="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi rem earum recusandae consectetur, eum amet odit beatae nihil nam dolorum a ipsa autem, optio praesentium, iure officiis iste dolorem fuga?</p>
+              <h6><span>2017</span></h6>
+            </div>
+            <div class="col-4">
+              <img src={'../images/movie.jpg'} alt="" width="100%"/>
+              <h6 class="mt-3">Capitan America 3</h6>
+              <p class="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi rem earum recusandae consectetur, eum amet odit beatae nihil nam dolorum a ipsa autem, optio praesentium, iure officiis iste dolorem fuga?</p>
+              <h6><span>2010</span></h6>
+            </div>
+          </div>
+        </div>
+      </div>
+  
+      <div class="container mt-3">
+        <div class="text-center">
+          <h3 class="subtitle">Best Comics</h3>
+          <p class="mb-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit dolore cupiditate vel ut quo alias.</p>
+          <div class="row">
+            <div class="col-2">
+              <img src={'../../images/comic.jpg'} alt="" width="100%"/>
+            </div>
+            <div class="col-2">
+              <img src={'../images/comic.jpg'} alt="" width="100%"/>
+            </div>
+            <div class="col-2">
+              <img src={'../images/comic.jpg'} alt="" width="100%"/>
+            </div>
+            <div class="col-2">
+              <img src={'../images/comic.jpg'} alt="" width="100%"/>
+            </div>
+            <div class="col-2">
+              <img src={'../images/comic.jpg'} alt="" width="100%"/>
+            </div>
+            <div class="col-2">
+              <img src={'../images/comic.jpg'} alt="" width="100%"/>
+            </div>
+          </div>
+        </div>
+      </div>
+</div>      
     );
   }
   
  
-const mapsStateToProps = (state) =>({hero: state.hero, stories: state.stories, comic: state.comic})
+const mapsStateToProps = (state) =>({...state})
 
 export default connect(mapsStateToProps)(Home);
 
 
-
-
-
-
-
-
-
-// function getData(){
-    //     axios.get(BASE_URL + CHARACTER_ROUTE)
-    // .then(
-    //     res=>{console.log(res.data.data.results[0],res.data.data.results[0].thumbnail,res.data.data.results[0].thumbnail.path)
-    //         setHero(res.data.data.results[0]);
-    //         setImage(res.data.data.results[0].thumbnail)
-    //     }
-    // )
-    // }
-
-// const appReducer = (state, action)=>{
-// switch (action.type){
-//        case HERO:
-//            return{
-//                res: [...state.res, action.payload]
-//            }; 
-//     };
-// };
-
-// const initialState = {
-//     users:[
-//         { }
-//     ]
-// }
-// const [state, dispatch] =  useReducer(
-//     appReducer,
-//     initialState
-// );
+{/* <div className="Home container-fluid"> */}
+        //     <div className="col">
+        //         <div className="row">
+        //         {props.hero?.name}
+        //         {props.hero?.description}
+        //         </div> 
+        //         <div className="row">
+        //         <img src={props.hero?.thumbnail?.path+'.'+props.hero?.thumbnail?.extension}/>
+        //             {props.comic?.map(com=>(
+        //             console.log(com.id)
+        //             ))}
+        //         </div>   
+        //     </div>
+        //         <p>
+        //     </p>
+        // </div>
--- src/pages/header/header.js
+++ src/pages/header/header.js
@@ -5,13 +5,42 @@ import "./header.scss";
 
 const Header = () => {
     return (
-        <header className="Header">
-            <nav className="navbar navbar-dark bg-dark">
-                <a href="" className="navbar-brand">
-                    My Favorite Hero
-                </a>
-            </nav>
-        </header>
+
+<header className="Header">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          {/* <img src='./logo.svg' alt="logo" width="30px">  Captain America </img> */}
+        </a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Features</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Pricing</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown link
+            </a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a className="dropdown-item" href="#">Action</a>
+              <a className="dropdown-item" href="#">Another action</a>
+              <a className="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+      </div>
+    </nav>
+  </header>
     );
   }
   
