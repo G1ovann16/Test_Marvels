f87b1fb722de5d415c4525232ff2bb1cb45d056d
G1ovann16 <60437581+G1ovann16@users.noreply.github.com>
2020-08-31 15:18:07 +0200

    display data comic and sotries

--- src/pages/components/Home/home.js
+++ src/pages/components/Home/home.js
@@ -12,104 +12,62 @@ useEffect(() => {
     
 }, []);
 
-const  comic = () =>({
-    
-})
 
     return (
     <div>    
-      <div class="bg-image">
-        <div class="container">
-          <div class="row">
-            <div class="col-6 my-auto">
-              <h1>Captain America</h1>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos pariatur quod totam doloremque, nulla veritatis dolorem? Perferendis, vero. Magni corrupti odit rerum maiores assumenda quam, quos repudiandae architecto in saepe.</p>
-              <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Primary link</a>
-              <a href="#" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Link</a>
+      <div className="bg-image">
+        <div className="container">
+          <div className="row">
+            <div className="col-6 my-auto">
+              <h1>{props.hero?.name}</h1>
+              <p>{props.hero?.description}</p>
+              <a href="#" className="btn btn-primary btn-lg active" role="button" aria-pressed="true">Primary link</a>
+              <a href="#" className="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Link</a>
             </div>
-            <div class="col-6">
+            <div className="col-6">
               <img src="captain.png" alt="" width="100%"/>
             </div>
           </div>
         </div>
       </div>
   
-      <div class="container mt-3">
-        <div class="text-center">
-          <h3 class="subtitle">Best Movies</h3>
-          <p class="mb-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit dolore cupiditate vel ut quo alias.</p>
-          <div class="row">
-            <div class="col-4">
-              <img src={'https://i.pinimg.com/736x/ba/9b/36/ba9b3623c9a639296e85f7ff09c3c205.jpg'} alt="" width="100%"/>
-              <h6 class="mt-3">Capitan America 1</h6>
-              <p class="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi rem earum recusandae consectetur, eum amet odit beatae nihil nam dolorum a ipsa autem, optio praesentium, iure officiis iste dolorem fuga?</p>
-              <h6><span>2020</span></h6>
-            </div>
-            <div class="col-4">
-              <img src={'../images/movie.jpg'} alt="" width="100%"/>
-              <h6 class="mt-3">Capitan America 2</h6>
-              <p class="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi rem earum recusandae consectetur, eum amet odit beatae nihil nam dolorum a ipsa autem, optio praesentium, iure officiis iste dolorem fuga?</p>
-              <h6><span>2017</span></h6>
-            </div>
-            <div class="col-4">
-              <img src={'../images/movie.jpg'} alt="" width="100%"/>
-              <h6 class="mt-3">Capitan America 3</h6>
-              <p class="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi rem earum recusandae consectetur, eum amet odit beatae nihil nam dolorum a ipsa autem, optio praesentium, iure officiis iste dolorem fuga?</p>
-              <h6><span>2010</span></h6>
+      <div className="container wrap mt-3">
+        <div className="text-center">
+          <h3 className="subtitle">Best Stories</h3>
+          <p className="mb-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit dolore cupiditate vel ut quo alias.</p>
+          <div className="row">
+            <div className="col-4">
+            {props.stories?.map(storie=>(
+              <h6 className="mt-3">{storie.title}</h6>
+              ))}
+              {/* {props.stories?.map(storie=>(
+              <p className="description">{storie.modified}</p>
+              ))} */}
+
             </div>
           </div>
         </div>
       </div>
   
-      <div class="container mt-3">
-        <div class="text-center">
-          <h3 class="subtitle">Best Comics</h3>
-          <p class="mb-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit dolore cupiditate vel ut quo alias.</p>
-          <div class="row">
-            <div class="col-2">
-              <img src={'../../images/comic.jpg'} alt="" width="100%"/>
+      <div className="container mt-3">
+        <div className="text-center">
+          <h3 className="subtitle">Best Comics</h3>
+          <p className="mb-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit dolore cupiditate vel ut quo alias.</p>
+          <div className="row">
+            <div className="col-2">
+              {props.comic?.map(com=>(
+                <img src={com.thumbnail?.path+'.'+com?.thumbnail?.extension} alt="" width="100%"/>
+              ))}
             </div>
-            <div class="col-2">
-              <img src={'../images/comic.jpg'} alt="" width="100%"/>
-            </div>
-            <div class="col-2">
-              <img src={'../images/comic.jpg'} alt="" width="100%"/>
-            </div>
-            <div class="col-2">
-              <img src={'../images/comic.jpg'} alt="" width="100%"/>
-            </div>
-            <div class="col-2">
-              <img src={'../images/comic.jpg'} alt="" width="100%"/>
-            </div>
-            <div class="col-2">
-              <img src={'../images/comic.jpg'} alt="" width="100%"/>
-            </div>
+        
           </div>
         </div>
       </div>
+      
 </div>      
-    );
-  }
+)}
   
  
 const mapsStateToProps = (state) =>({...state})
 
 export default connect(mapsStateToProps)(Home);
-
-
-{/* <div className="Home container-fluid"> */}
-        //     <div className="col">
-        //         <div className="row">
-        //         {props.hero?.name}
-        //         {props.hero?.description}
-        //         </div> 
-        //         <div className="row">
-        //         <img src={props.hero?.thumbnail?.path+'.'+props.hero?.thumbnail?.extension}/>
-        //             {props.comic?.map(com=>(
-        //             console.log(com.id)
-        //             ))}
-        //         </div>   
-        //     </div>
-        //         <p>
-        //     </p>
-        // </div>
